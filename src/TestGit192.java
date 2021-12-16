5b08e8b82c30490df0d95a380648fdcf15795c92
godYYd <1963965096@qq.com>
2021-12-16 16:37:46 +0800

    杨亿栋的修改

--- src/TestGit192.java
+++ src/TestGit192.java
@@ -7,6 +7,7 @@ public class TestGit192 {
         stuInfo("19406040228","徐逸群");
         stuInfo("19406040219","胡智岚");
         stuInfo("19406040229","郭佳俊");
+        stuInfo("19406040220","杨亿栋");
     }
 
     public static void stuInfo(String sid, String sname) {
